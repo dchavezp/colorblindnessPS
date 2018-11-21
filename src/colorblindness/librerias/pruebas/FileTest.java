9e5bde417b1e3a43cc3dbd9a366f027bdb027e4d
davidhqaqp <david.hq.aqp@gmail.com>
2018-11-21 12:03:59 -0500

    subiendo los demas metodos y tambien actualizando el archivo gitignore

--- src/colorblindness/librerias/pruebas/FileTest.java
+++ src/colorblindness/librerias/pruebas/FileTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class FileTest {
 
 	//PRUEBAS DE EQUIVALENCIA
-	
+
 	@Test
 	//PARA CUANDO LA RUTA ES CORRECTA
 	public void File_CP_1() {
@@ -17,6 +17,33 @@ public class FileTest {
 		File file=new File("Q:\\UNSA\\PSoft\\manzana.jpg");
 		String valorActual= file.getPath();
 		System.out.println("dsad"+valorActual);
+		assertEquals(resultadoEsperado, valorActual);
+	}
+	//PARA CUANDO LA RUTA ES ERRONEA
+	@Test
+	public void File_CP_2() {
+		String  resultadoEsperado="Q:\\UNSA\\PSoft\\manzana.jpg";
+		File file=new File("Q:\\UNSA\\PSoft\\images\\manzana.jpg");
+		String valorActual= file.getPath();
+		System.out.println(valorActual);
+		assertEquals(resultadoEsperado, valorActual);
+	}
+	//PARA CUANDO LA RUTA ES UN NUMERO
+	@Test
+	public void File_CP_3() {
+		String  resultadoEsperado="Q:\\UNSA\\PSoft\\manzana.jpg";
+		File file=new File("123213");
+		String valorActual= file.getPath();
+		System.out.println(valorActual);
+		assertEquals(resultadoEsperado, valorActual);
+	}
+	//PARA CUANDO LA RUTA ESTA VACIA
+	@Test
+	public void File_CP_4() {
+		String  resultadoEsperado="Q:\\UNSA\\PSoft\\manzana.jpg";
+		File file=new File("");
+		String valorActual= file.getPath();
+		System.out.println(valorActual);
 		assertEquals(resultadoEsperado, valorActual);
 	}
 
