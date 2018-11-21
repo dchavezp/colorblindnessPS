6a660d6a573a4942bf980d09f615bd6349fe0d48
davidhqaqp <david.hq.aqp@gmail.com>
2018-11-21 11:55:03 -0500

    subiendo primer caso de prueba de la clase file y tambien el archivo gitignore

--- src/colorblindness/librerias/pruebas/FileTest.java
+++ src/colorblindness/librerias/pruebas/FileTest.java
@@ -2,13 +2,22 @@ package colorblindness.librerias.pruebas;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 public class FileTest {
 
+	//PRUEBAS DE EQUIVALENCIA
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	//PARA CUANDO LA RUTA ES CORRECTA
+	public void File_CP_1() {
+		String  resultadoEsperado="Q:\\UNSA\\PSoft\\manzana.jpg";
+		File file=new File("Q:\\UNSA\\PSoft\\manzana.jpg");
+		String valorActual= file.getPath();
+		System.out.println("dsad"+valorActual);
+		assertEquals(resultadoEsperado, valorActual);
 	}
 
 }
