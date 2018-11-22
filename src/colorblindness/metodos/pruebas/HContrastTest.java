9830c8656fa0062b317d9b6a750740fbdcb94a71
dchavezp <dschp229@gmail.com>
2018-11-22 14:52:52 -0500

    añadiend hcontrast

---
+++ src/colorblindness/metodos/pruebas/HContrastTest.java
@@ -0,0 +1,79 @@
+package colorblindness.metodos.pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HContrastTest {
+
+	@Test
+	public void cp1Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp2Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp3Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp4Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp5Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp6Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp7Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp8Test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp9Test() {
+		fail("Not yet implemented");
+	}
+	//Casos de Prueba Valor limite
+	@Test
+	public void cp1VLTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp2VLTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp3VLTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp4VLTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp5VLTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp6VLTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp7VLTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void cp8VLTest() {
+		fail("Not yet implemented");
+	}
+
+}
